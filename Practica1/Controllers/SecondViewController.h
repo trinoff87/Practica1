41dc0ea1736791ba907c03051ea57ba0ad3cee29
Jose Trinidad Fajardo Flores <trinof@Joses-Mac.local>
2017-11-01 17:52:59 -0700

    Added initial story structure

---
+++ Practica1/Controllers/SecondViewController.h
@@ -0,0 +1,13 @@
+//
+//  SecondViewController.h
+//  Practica1
+//
+//  Created by Jose Trinidad Fajardo Flores on 11/1/17.
+//  Copyright © 2017 Jose Trinidad Fajardo Flores. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+@end
